1afd47b1e0ecb21461b0c3a1f8460fd95273e1a7
aliciastudies <6722@holbertonstudents.com>
2023-09-21 13:35:10 +1000

    Task 5 - copy - cut - paste

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = str1 + ' ' + str2# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str3 = str1 + ' ' + str2
 print(f"Welcome to {str3}!")
