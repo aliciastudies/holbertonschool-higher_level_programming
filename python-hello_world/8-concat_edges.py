819d3f9589567ec88a028aa322b7b2fb11f92224
aliciastudies <6722@holbertonstudents.com>
2023-09-21 14:08:47 +1000

    removed extra var

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str_edge = str[39:66]
-print(f"{str_edge} with Python")
+print(f"{str[39:66]} with Python")
