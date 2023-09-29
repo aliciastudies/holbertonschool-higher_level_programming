954a62c1d2c5ed92e82133b0511114878b5aeb05
aliciastudies <6722@holbertonstudents.com>
2023-09-29 15:45:21 +1000

    update: removed f-string

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
-            print(f"Fizzbuzz", end=" ")
+            print("Fizzbuzz", end=" ")
         elif x % 3 == 0:
-            print(f"Fizz", end=" ")
+            print("Fizz", end=" ")
         elif x % 5 == 0:
-            print(f"Buzz", end=" ")
+            print("Buzz", end=" ")
         else:
             print(x, end=" ")
