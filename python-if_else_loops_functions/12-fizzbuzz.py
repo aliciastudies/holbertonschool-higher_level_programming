eaac245bd7d803cddb0939a20150de7fd546dac2
aliciastudies <6722@holbertonstudents.com>
2023-09-29 15:31:52 +1000

    Task 12 - Fizz Buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print(f"Fizzbuzz", end=" ")
+        elif x % 3 == 0:
+            print(f"Fizz", end=" ")
+        elif x % 5 == 0:
+            print(f"Buzz", end=" ")
+        else:
+            if x < 100:
+                print(x, end=" ")
+            else:
+                print(x)
