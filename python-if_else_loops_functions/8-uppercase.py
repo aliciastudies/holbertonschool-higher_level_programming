83ef7b0454d7c1ab60a60da370712648357d8d93
aliciastudies <6722@holbertonstudents.com>
2023-09-29 13:25:59 +1000

    Task 8 - func prints str in upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+
+
+def uppercase(str):
+    for i in str:
+        if islower(i):
+            i = chr(ord(i) - 32)
+        print("{0}".format(i), end="")
+    print("")
