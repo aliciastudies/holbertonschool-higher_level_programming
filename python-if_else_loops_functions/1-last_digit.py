01131ef7b4560a3dc67da89e77cd4d10a023ea49
aliciastudies <6722@holbertonstudents.com>
2023-09-29 14:44:28 +1000

    update: refractored to pass linter

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,24 +2,24 @@
 import random
 number = random.randint(-10000, 10000)
 
-# print(number)
-def is_last(number: int):
-    if number >= 10:
-        last_digit = number % 10
-        return(last_digit)
-    elif number >= 0 and number < 10:
-        last_digit = number
-        return(last_digit)
+
+def print_message(a, b):
+    if b > 5:
+        print(f"Last digit of {a} is {b} and is greater than 5")
+    elif a == 0:
+        print(f"Last digit of {a} is {b} and is 0")
     else:
-        if number < 0:
-            last_digit = number % -10
-            return(last_digit)
+        print(f"Last digit of {a} is {b} and is less than 6 and not 0")
+    return 0
 
 
-def if_last(last_digit: str):
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit != 0 and last_digit < 6:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-else:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+# print(number)
+last_digit = number % 10
+if number >= 10:
+    print_message(number, last_digit)
+if number < 0:
+    last_digit = number % -10
+    print_message(number, last_digit)
+if number >= 0 and number < 10:
+    last_digit = number
+    print_message(number, last_digit)
