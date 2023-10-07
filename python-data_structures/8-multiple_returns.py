cd5151f9d1b535647c7c8e26d61f691fe3ef5097
aliciastudies <6722@holbertonstudents.com>
2023-10-07 16:59:43 +1100

    feat: func returns tuple with multiple data types

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        return 0, None
+    else:
+        first_char = sentence[0]
+        return (len(sentence), first_char)
