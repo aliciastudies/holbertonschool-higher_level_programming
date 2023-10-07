b302da2d1cd0c872f04cb9be575fa31e20cc3f84
aliciastudies <6722@holbertonstudents.com>
2023-10-07 12:15:45 +1100

    chmod'd

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        print("{0}".format(my_list[i]))
+        print(int("{0:d}".format(my_list[i])))
