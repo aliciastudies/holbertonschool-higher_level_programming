3f68530986a6cf064ba1d86236820da38fdbe4da
aliciastudies <6722@holbertonstudents.com>
2023-10-10 11:52:39 +1100

    update: added documentation and fixed formatting

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defining a Square
+"""
+
+
+class Square:
+    """
+    Adding a private attribute
+    """
+    def __init__(self, size=0):
+        """
+        adding TypeError and ValueError
+        """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
