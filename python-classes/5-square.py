67abe4bf463913f18a2470337f2a5f085d5ebd92
aliciastudies <6722@holbertonstudents.com>
2023-10-10 16:28:14 +1100

    feat: added printing func

---
+++ python-classes/5-square.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+Defining class Square:
+private attribute size, getter and setter, public method
+"""
+
+
+class Square:
+    """
+    Creating Squares
+
+    Attributes:
+        __size: (int): Size of square
+
+    Methods:
+        __init__(self, size=0): initiliases square with default 0
+        area(self): calculates current square area
+        my_print(self): prints square with the character #
+
+    """
+    def __init__(self, size=0):
+        """
+        Initialises new Square instance
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Function to get value of __size
+
+        Returns:
+            __size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Function to set value of __size
+
+        Args:
+            value (int): size of square
+
+        Raises:
+            TypeError: value must be an int
+            ValueError: value must be greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Function that calculates the current square area
+
+        Return:
+            Current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Function prints square with the character #
+        """
+        for row in range(self.__size):
+            if self.__size == 0:
+                print()
+            for index in range(self.__size):
+                print("#", end="")
+            print()
