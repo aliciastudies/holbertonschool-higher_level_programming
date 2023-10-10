200a609f9b31e1d38c4ecbbb67f441ec0f90a918
aliciastudies <6722@holbertonstudents.com>
2023-10-10 12:00:03 +1100

    update: fixed formatting

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,8 +12,8 @@ class Square:
         """
         adding TypeError and ValueError
         """
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
