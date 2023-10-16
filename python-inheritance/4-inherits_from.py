148f622b1e2595b1b2b45b791410d04b0d304212
aliciastudies <6722@holbertonstudents.com>
2023-10-16 16:00:23 +1100

    feat: only subclass of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function that returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class,
+else False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Args:
+        obj: object
+        a_class: class
+
+        Return: True if exact match, else False
+    """
+    if isinstance(obj, a_class) is True:
+        if type(obj) is not a_class:
+            return True
+        return False
