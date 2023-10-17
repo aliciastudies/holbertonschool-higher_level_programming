894b002059a74fbf09953102601fbbc78e2062ef
aliciastudies <6722@holbertonstudents.com>
2023-10-17 11:42:13 +1100

    feat: def area(self)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    currently empty
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
