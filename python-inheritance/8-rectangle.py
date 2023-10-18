9637bbc5a65528b624bb6c90ae2d86584de97534
aliciastudies <6722@holbertonstudents.com>
2023-10-18 17:20:04 +1100

    feat: class inherits from parent

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+"""
+class Rectangle inherting from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Creating Rectangles
+    """
+    def __init__(self, width, height):
+        """
+        Function initialises new Rectangle instance
+
+        Args:
+            width: int
+            height: int
+        """
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
