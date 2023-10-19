2303e83ee5eb66726524a731cb32e10f783c9bf4
aliciastudies <6722@holbertonstudents.com>
2023-10-19 13:06:21 +1100

    feat: add str()

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+class Rectangle inherting from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Creating Rectangles
+    """
+    def __init__(self, width, height):
+        """
+        Function initialises new Rectangle instance
+
+        Args:
+            width: int
+            height: int
+        """
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
+
+    def area(self):
+        """
+        Returns area of rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns description of rectangle
+        """
+        return f"[{type(self).__name__}] {self.__width}/{self.__height}"
