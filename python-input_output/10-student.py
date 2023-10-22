6eb9f3e69bcd57ab3c0e32c74c88fb77f8f805a4
aliciastudies <6722@holbertonstudents.com>
2023-10-22 20:36:13 +1100

    feat: class Student - return attr names only

---
+++ python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student
+"""
+
+
+class Student:
+    """
+    Define class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        instantiate attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for at in attrs:
+            if hasattr(self, at):
+                new_dict[at] = getattr(self, at)
+        return new_dict
