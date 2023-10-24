42a349d9ac97414c07d82b14bf720b8c332ecae8
aliciastudies <6722@holbertonstudents.com>
2023-10-24 20:40:58 +1100

    feat: added __str__()

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,11 @@ class Rectangle(Base):
             for j in range(self.width):
                 print("#", end="")
             print("")
+
+    def __str__(self):
+        """
+        Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        class_name = type(self).__name__
+        return f"[{type(self).__name__}] ({self.id}) {self.x}/{self.y}\
+            - {self.width}/{self.height}"
