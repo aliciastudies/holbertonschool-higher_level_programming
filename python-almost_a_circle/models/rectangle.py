2a4f78847e3df9a2708dcaaa6d33e01c94a6af1c
aliciastudies <6722@holbertonstudents.com>
2023-10-24 15:41:43 +1100

    update: assigned value

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,13 +9,12 @@ class Rectangle(Base):
     """
     class Rectangle inherits from class Base
     """
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -39,7 +38,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.__x = x
+        self.__x = value
 
     @property
     def y(self):
@@ -47,4 +46,4 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.__y = y
+        self.__y = value
