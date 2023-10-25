cbe27d05069ce4f219c93dcaec18efd09ca28be0
aliciastudies <6722@holbertonstudents.com>
2023-10-25 15:53:44 +1100

    feat: added to_dictionary(self)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -67,3 +67,9 @@ class Square(Rectangle):
                 self.y = kwargs["y"]
             except KeyError:
                 pass
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of a Square
+        """
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
