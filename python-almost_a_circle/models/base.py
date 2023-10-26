6c7f77cc067629a6c566f4abec2a7aa92dee7f38
aliciastudies <6722@holbertonstudents.com>
2023-10-26 11:11:31 +1100

    feat: added save_to_file()

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,3 +33,18 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file
+        """
+        filename = cls.__name__ + ".json"
+        list_dict = []
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                pass
+            else:
+                for obj in list_objs:
+                    list_dict.append(obj.to_dictionary())
+                    jsonfile.write(cls.to_json_string(list_dict))
