68c37c73dab0e62c8c0cb48432faf65860c1a2e0
aliciastudies <6722@holbertonstudents.com>
2023-11-21 14:56:40 +1100

    fix: added ;

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON localhost.* TO 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd'
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
