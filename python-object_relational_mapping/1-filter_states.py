666c7969c63d805f631cb82d9c708689e7c960d6
aliciastudies <6722@holbertonstudents.com>
2023-11-28 22:55:36 +1100

    fix: pass pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE BINARY name \
         LIKE 'N%' ORDER BY id;")
