b533c256757a10d16f17a9a96f0b5109bcd7e637
aliciastudies <6722@holbertonstudents.com>
2023-11-28 22:54:05 +1100

    feat: Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" lists all states with a name starting with N from database """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name \
+        LIKE 'N%' ORDER BY id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
