2169942f729915f2bda2b2d97f00d19ad3fb6412
aliciastudies <6722@holbertonstudents.com>
2023-11-29 17:39:23 +1100

    feat: First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+""" contains class definitions of a State and an instance Base """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ create class State & inherits from Base """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
