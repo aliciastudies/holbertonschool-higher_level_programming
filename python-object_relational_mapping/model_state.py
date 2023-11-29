90cfcb4f134413d68c016d1c7ff5d6d0801a886d
aliciastudies <6722@holbertonstudents.com>
2023-11-29 17:44:22 +1100

    fix: added shebang

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ contains class definitions of a State and an instance Base """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
